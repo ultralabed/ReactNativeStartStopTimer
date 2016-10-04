2e4e85eafd1daf631a09f7db21abc944f116a38c
Edward Shih <ultralabed@gmail.com>
2016-10-04 11:42:36 +0800

    Add simple layout with helper function.

--- index.ios.js
+++ index.ios.js
@@ -9,12 +9,23 @@ import {
 let StopWatch = React.createClass ({
   render:  function() {
     return (
-      <View>
-        <Text>
-          00:00.00
-        </Text>
-        { this.startStopButton() }
-        { this.lapButton() }
+      <View style={ styles.container }>
+        <View style={ [styles.header, this.border('yellow')] }>
+          <View style={ this.border('red') }>
+            <Text>
+              00:00.00
+            </Text>
+          </View>
+          <View style={ this.border('green') }>
+            { this.startStopButton() }
+            { this.lapButton() }
+          </View>
+        </View>
+        <View style={ [styles.footer, this.border('blue')] }>
+          <Text>
+            I am a list of Laps
+          </Text>
+        </View>
       </View>
     )},
     startStopButton: function() {
@@ -34,11 +45,27 @@ let StopWatch = React.createClass ({
           </Text>
         </View>
       )
+    },
+    border: function(color) {
+      return {
+        borderColor: color,
+        borderWidth: 4
+      }
     }
 })
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Fill the entire the screen
+    alignItems: 'stretch'
 
+  },
+  header: { //Yellow
+    flex: 1
+  },
+  footer: { //Blue
+    flex: 1
+  }
 });
 
 AppRegistry.registerComponent('stopwatch', () => StopWatch);
